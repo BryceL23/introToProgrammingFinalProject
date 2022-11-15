2b8bcdba54e3f726c08b701f251b3abf9af0d7ee
BryceL23 <115042875+BryceL23@users.noreply.github.com>
2022-11-15 12:47:55 -0800

    new file

---
+++ main.py
@@ -0,0 +1,13 @@
+# Overview 
+    # Fantasy Football Data Snatcher, allows for a faster and more efficient way to take data from fantasy football websites to excel files to later be analysed. 
+
+    # During my statistics class earlier this school year we were tasked with a project to create graphs and analyze data on a topic of our choice. During the time when this project was introduced I was still workign on that project and was unable to find a single website that would have all the data poitns I was looking for and as a result had to gather data from multiple sites into a single excel file and format it correctly so I was usable. I wanted to create a solution for this problem and would like to create a sceen scraper to bring the new data after each week to the excel spreadsheet ready to be turned into graphs so trends for players and teams can be analyzed.
+
+# Sources:
+    # look at automatetheboringstuff.com
+# Libraries:
+
+# Steps of Construction:
+    # Find the website I wish to draw data from
+    # write the code that will allow me to record the data from the website and into a file which could be used in a later use in an excel file
+    # make a path to send the code to an excel or google sheet 
