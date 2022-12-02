0cdd23c068a88419fb453c5442ff353cf09792aa
BryceL23 <115042875+BryceL23@users.noreply.github.com>
2022-12-02 11:36:19 -0800

    day 12 2

--- newmain.py
+++ newmain.py
@@ -1,10 +1,26 @@
-# look at scrap.py 
-# look at PDFextract.py in his code (look at links in the source code) geeks for geeks writing to an excel sheet 
+# # look at scrap.py 
+# # look at PDFextract.py in his code (look at links in the source code) geeks for geeks writing to an excel sheet 
 
+# import requests, bs4
+# res = requests.get('https://nostarch.com')
+# res.raise_for_status()
+# noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
+# print(type(noStarchSoup))
+
+# print(noStarchSoup)
+
+############## open website with selenium and webdriver ###################
 import requests, bs4
-res = requests.get('https://nostarch.com')
-res.raise_for_status()
-noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
-print(type(noStarchSoup))
 
-print(noStarchSoup)
+URL = 'https://www.pro-football-reference.com/years/2022/rushing.htm'
+
+# get info from website
+res = requests.get(URL)
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+print(type(soup))
+print(soup)
+elem = soup.select('tbody')
+print(len(elem))
+print(str(elem))
+# dump into excel
