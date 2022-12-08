557ea3089c182b217ce8769cfc48e21a3d668caf
BryceL23 <115042875+BryceL23@users.noreply.github.com>
2022-12-07 22:36:15 -0800

    new work

--- newmain12_6.py
+++ newmain12_6.py
@@ -3,60 +3,79 @@
 
 ############## open website with selenium and webdriver ###################
 from time import sleep
-import xlwt
+# import xlwt
 from xlwt import Workbook
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
+# allows for the webpage used in the project to be accessed and opened and closed through the software, not needing to be manually controlled
+# from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 
 # scrape page
 URL = 'https://www.pro-football-reference.com/years/2022/rushing.htm'
+# url to draw the data from for the project 
 
 chrome_driver = "Code\Projects\chromedriver.exe"
 driver = webdriver.Chrome(chrome_driver)
+# the driver used to control the webpages will open the url in a chrome browser
 driver.get(URL)
+# loads up or starts the search engine
 sleep(5)
+# delays the execution of the task for the amount of seconds imput into the ()
+
 # spans = driver.find_element(By.TAG_NAME, "span")
 # print(type(spans))
+
 searchresults = driver.find_elements(By.XPATH,"//td[contains(@class,'left')]")
+# defining the variable of searchrsesults which will locate the selected data from the 'left' class and store it until it is called to write it somewhere else
 searchresults1 = driver.find_elements(By.XPATH,"//td[contains(@class,'right')]")
+# defining the variable of searchrsesults which will locate the selected data from the 'left' class and store it until it is called to write it somewhere else
+
 # searchresults = driver.find_elements(By.XPATH,"//span[contains(@class,'ng-binding')]")
 
+
 datafound = []
+# stores the list for the different sets of data 
 datafound1 =[]
+# stores the list for the different sets of data 
+
 for i in searchresults:
     if len(i.text) > 0:
         datafound.append(i.text)
     if len(datafound) > 162:
         break
-
+# gather the data within the provided source up to the determined number of string values and store them to be later written to and excel file when later called upon, the loop runs within itself until the desirved info is collected and then it breaks
 for i in searchresults1:
     if len(i.text) > 0:
-        datafound.append(i.text)
-    if len(datafound) > 162:
+        datafound1.append(i.text)
+    if len(datafound1) > 127:
         break
-print(datafound)
+    # gather the data within the provided source up to the determined number of string values and store them to be later written to and excel file when later called upon, the loop runs within itself until the desirved info is collected and then it breaks
+
 
 #################### write to excel #########################
 wb = Workbook()
 
 sheet1 = wb.add_sheet("2022 Rushing Totals")
-
+# maps the first sheet within the excel file and titles it with the preset name 2022 Rushing Totals 
 
 sheet1.write(0,0, "Name")
 sheet1.write(0,1, "Team")
 sheet1.write(0,2, "Position")
 sheet1.write(0,3, "Attempts")
 sheet1.write(0,4, "Yards")
+# within sheet1 of the excel file the collumns 1 through 5 are named as above 
 
 for i in range(0,162):
     sheet1.write((i)//3+1,i%3, datafound[i])
+    # for the string value in the range it is written within its corresponding column from the list values found in datafound until the determined limit of values is reached
 # modulo takes remainder so it does i/3 the puts the remainder in that row 
 
-for i in range(0,162):
+for i in range(0,127):
     sheet1.write((i)//3+4,i%3, datafound1[i])
-# what is added is where the column of new data input begins
+        # for the string value in the range it is written within its corresponding column from the list values found in datafound until the determined limit of values is reached
+# # what is added is where the column of new data input begins
 wb.save('xlwt_Rushing_Totals_Data1.xls')
+# the file with the newly entered information is then saved within my local disk --> github --> intro to programming --> final project file path
 
 
 
