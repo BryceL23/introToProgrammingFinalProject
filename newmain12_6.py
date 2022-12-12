f0cbf397bdced8e1a6c25951184ad4e775684277
BryceL23 <115042875+BryceL23@users.noreply.github.com>
2022-12-12 14:31:54 -0800

    working final

--- newmain12_6.py
+++ newmain12_6.py
@@ -42,13 +42,13 @@ for i in searchresults:
     if len(i.text) > 0:
         datafound.append(i.text)
 
-    if len(datafound) > 162:
+    if len(datafound) > 156:
         break
 # gather the data within the provided source up to the determined number of string values and store them to be later written to and excel file when later called upon, the loop runs within itself until the desirved info is collected and then it breaks
 for i in searchresults1:
     if len(i.text) > 0:
         datafound1.append(i.text)
-    if len(datafound1) > 162:
+    if len(datafound1) > 572:
         break
     # gather the data within the provided source up to the determined number of string values and store them to be later written to and excel file when later called upon, the loop runs within itself until the desirved info is collected and then it breaks
 
@@ -59,20 +59,34 @@ wb = Workbook()
 sheet1 = wb.add_sheet("2022 Rushing Totals")
 # maps the first sheet within the excel file and titles it with the preset name 2022 Rushing Totals 
 
-sheet1.write(0,1, "Name")
-sheet1.write(0,2, "Team")
-sheet1.write(0,3, "Position")
-sheet1.write(0,5, "Attempts")
-sheet1.write(0,6, "Yards")
+sheet1.write(0,0, "Name")
+sheet1.write(0,1, "Team")
+sheet1.write(0,2, "Position")
+sheet1.write(0,3,"Age")
+sheet1.write(0,4, "Games Played")
+sheet1.write(0,5, "Games Started")
+sheet1.write(0,6, "Attempts")
+sheet1.write(0,7, "Yards")
+sheet1.write(0,8, "Touchdowns Scored")
+sheet1.write(0,9, "First Downs Rushing")
+sheet1.write(0,10,"Longest Rush Attempt")
+sheet1.write(0,11,"Rushing Yards Per Attempt")
+sheet1.write(0,12,"Rushing Yards Per Game")
+sheet1.write(0,13,"Fumbles")
+
+
+
+
 # within sheet1 of the excel file the collumns 1 through 5 are named as above 
 
-for i in range(0,162):
+for i in range(0,156):
     sheet1.write((i)//3+1,i%3, datafound[i])
     # for the string value in the range it is written within its corresponding column from the list values found in datafound until the determined limit of values is reached
 # modulo takes remainder so it does i/3 the puts the remainder in that row 
 
-for i in range(0,162):
-    sheet1.write((i)//3+4,i%3, datafound1[i])
+for i in range(0,572):
+    sheet1.write((i)//11+1,i%11+3, datafound1[i])
+        # tell it where the data should go 
         # for the string value in the range it is written within its corresponding column from the list values found in datafound until the determined limit of values is reached
 # # what is added is where the column of new data input begins
 wb.save('xlwt_Rushing_Totals_Data1.xls')
